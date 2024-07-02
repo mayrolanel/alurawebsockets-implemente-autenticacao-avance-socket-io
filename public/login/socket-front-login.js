03fd8c6d6caee63dbdb18a34b120cfe504b5916e
mayro.belizario <mayro.belizario@trinusco.com.br>
2024-07-02 19:45:26 -0300

    aula-02: implementando o login

---
+++ public/login/socket-front-login.js
@@ -0,0 +1,20 @@
+const socket = io();
+
+function emitirAutenticarUsuario(dados) {
+    socket.emit("autenticar_usuario", dados)
+}
+
+socket.on("autenticacao_sucesso", () => {
+    alert("Usuario autenticado com sucesso")
+    window.location.href = "/"
+})
+
+socket.on("autenticacao_erro", () => {
+    alert("Erro na autenticao")
+})
+
+socket.on("usuario_nao_encontrado", () => {
+    alert("Usuario não encontrado")
+})
+
+export { emitirAutenticarUsuario }
